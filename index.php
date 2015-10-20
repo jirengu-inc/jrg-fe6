611e76dcabeb3e57dcc7e4e7b6e7e1eec7ade784
hunger <hunger@jirengu.com>
2015-10-20 17:59:06 +0800

    m

--- index.php
+++ index.php
@@ -71,9 +71,9 @@
                   
                     while (false !== ($file = readdir($handle)))   
                     {  
-                        if (is_dir($path.$file) && $file != '.' && $file !='..' && substr($file, 0, 1) !='.')  
+                        if (is_dir($path.$file) && $file != '.' && $file !='..' && substr($file, 0, 1) !='.' && $file != 'Procfile' && $file != 'README.md' && $file != 'index.php' && $file != 'composer.json')  
                             printSubDir($file, $path, $queue);  
-                        else if ($file != '.' && $file !='..'  && substr($file, 0, 1) !='.')  
+                        else if ($file != '.' && $file !='..'  && substr($file, 0, 1) !='.' && $file != 'Procfile' && $file != 'README.md' && $file != 'index.php' && $file != 'composer.json')  
                             $queue[] = $file;  
                     }  
                       
