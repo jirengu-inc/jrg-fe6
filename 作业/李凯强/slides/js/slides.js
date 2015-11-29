408ea5e863d7169d9dbfacdfcaa6e52d34aca775
李凯强 <921085426@qq.com>
2015-11-30 02:01:11 +0800

    add slides tab button

--- 作业/李凯强/slides/js/slides.js
+++ 作业/李凯强/slides/js/slides.js
@@ -6,6 +6,31 @@ $.fn.slides=function(isCycle){
 		var length=$currentUl.children().length;
 		var liWidth=$currentUl.children().first().width();
 		$currentUl.width(liWidth*length);
+		var $tab=$currentUl.closest('.slides').find('.tab').children();
+		var $slides=$currentUl.closest('.slides');
+		$slides.on('mouseover',function(e){
+			$(this).find('.leftBtn,.rightBtn').css({
+				'display': 'inline-block',
+				'opacity': 0
+			});
+			$(this).find('.leftBtn,.rightBtn').animate({
+				'opacity': 0.7
+			});
+		});
+		$slides.on('mouseout',function(e){
+			$(this).find('.leftBtn,.rightBtn').css({
+				'display': 'none'
+			});
+		})
+		function tabStatus(){
+			$tab.each(function(i,obj){
+				if(i++==currentIndex){
+					$(obj).addClass('active');
+					$(obj).siblings().removeClass('active');
+				}
+			})
+		}
+		$tab.first().addClass('active');
 		if(!isCycle){
 			setTimeout(function(){
 				currentIndex++;
@@ -14,6 +39,7 @@ $.fn.slides=function(isCycle){
 				$currentUl.animate({
 					'left': -currentIndex*liWidth
 				});
+				tabStatus();
 				setTimeout(arguments.callee,1000);
 			},1000);
 		}
